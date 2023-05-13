b62af5ee7941cb08ac2028087d2e0fe3573d3d14
YoelEigner <35924447+YoelEigner@users.noreply.github.com>
2023-05-13 12:47:02 +0300

    added readme file

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -10,7 +10,7 @@ authRouter.post('/login', async (req, res) => {
         let user = await getUser(username, password)
         if (user) {
             const token = await jwt.sign({ name: username }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '12h' });
-            res.json({ "token": token, username: user.username, password: user.password, timestamp: user.timestamp })
+            res.json({ "token": token, username: user.username, timestamp: user.timestamp })
         }
         else {
             return res.status(400).send('Incorrect Username or password')
@@ -25,7 +25,7 @@ authRouter.get('/get-user', async (req, res) => {
         let { username, password } = req.body
         let user = await getUser(username, password)
         if (user) {
-            res.json({ username: user.username, password: user.password, timestamp: user.timestamp })
+            res.json({ username: user.username, timestamp: user.timestamp })
         }
         else {
             return res.status(400).send('Incorrect Username or password')
